659ac4e8d06460a8431108ad541f20ac3e6bf139
IntanSari6 <intansari250920@gmail.com>
2023-07-15 15:22:03 +0700

    menambahkan layouting

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,13 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Halaman Post</h1>
-</body>
-</html>
+
+@extends('layouts.main')
+
+@section('container')
+<article>
+<h2>{{ $post["title"] }}l</h2>
+<h5>{{ $post["author"] }}</h5>
+<p>{{ $post["body"] }}</p>
+</article>
+
+<a href="/blog">Back to posts</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,61 @@ Route::get('/', function () {
 });
 
 Route::get('/a', function () {
-    return view('home');
+    return view('home', ["title" => "Home"]);
 });
 
 Route::get('/about', function () {
-    return view('about', ["name" => "Intan", "Email" => "intan@gmail.com", "image" => "rh.jfif"]);
+    return view('about', [ "title" => "About", "name" => "Intan", "Email" => "intan@gmail.com", "image" => "rh.jfif"]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('post');
+    $blog_posts =[
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Intan Sari",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia quis aut perspiciatis facere nostrum modi eveniet dolores odio amet possimus iusto, in atque quos, ducimus sint obcaecati. Reiciendis, velit aperiam neque libero deserunt quam qui iusto distinctio cum, ab pariatur optio reprehenderit, amet provident id? Fuga esse, quidem voluptatem, tenetur rerum neque eaque eveniet quos ullam quia aliquid necessitatibus cumque commodi reprehenderit dignissimos quisquam quae dolores, hic vel molestiae molestias consectetur accusamus totam ipsum! Labore odio odit magnam voluptas."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Florence",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia quis aut perspiciatis facere nostrum modi eveniet dolores odio amet possimus iusto, in atque quos, ducimus sint obcaecati. Reiciendis, velit aperiam neque libero deserunt quam qui iusto distinctio cum, ab pariatur optio reprehenderit, amet provident id? Fuga esse, quidem voluptatem, tenetur rerum neque eaque eveniet quos ullam quia aliquid necessitatibus cumque commodi reprehenderit dignissimos quisquam quae dolores, hic vel molestiae molestias consectetur accusamus totam ipsum! Labore odio odit magnam voluptas."
+        ],
+    ];
+    return view('posts', ["title" => "posts",
+     "posts" => $blog_posts]);
+
+});
+
+
+ //Halaman Single slug
+
+Route::get('posts/{slug}', function($slug){
+    $blog_posts =[
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Intan Sari",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia quis aut perspiciatis facere nostrum modi eveniet dolores odio amet possimus iusto, in atque quos, ducimus sint obcaecati. Reiciendis, velit aperiam neque libero deserunt quam qui iusto distinctio cum, ab pariatur optio reprehenderit, amet provident id? Fuga esse, quidem voluptatem, tenetur rerum neque eaque eveniet quos ullam quia aliquid necessitatibus cumque commodi reprehenderit dignissimos quisquam quae dolores, hic vel molestiae molestias consectetur accusamus totam ipsum! Labore odio odit magnam voluptas."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Florence",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia quis aut perspiciatis facere nostrum modi eveniet dolores odio amet possimus iusto, in atque quos, ducimus sint obcaecati. Reiciendis, velit aperiam neque libero deserunt quam qui iusto distinctio cum, ab pariatur optio reprehenderit, amet provident id? Fuga esse, quidem voluptatem, tenetur rerum neque eaque eveniet quos ullam quia aliquid necessitatibus cumque commodi reprehenderit dignissimos quisquam quae dolores, hic vel molestiae molestias consectetur accusamus totam ipsum! Labore odio odit magnam voluptas."
+        ],
+    ];
+
+
+$new_post = [];
+foreach($blog_posts as $post){
+    if($post["slug"] === $slug){
+        $new_post = $post;
+    }
+}
+
+    return view('post', ["title" => "Single Post", "post" => $new_post]);
 });
