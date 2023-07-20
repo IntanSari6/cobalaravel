101c2312fd5dfc88034bd0966736cc53cc67c2de
IntanSari6 <intansari250920@gmail.com>
2023-07-20 21:33:44 +0700

    Authorization

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardPostController;
+use App\Http\Controllers\AdminCategryController;
 
 
 
@@ -92,3 +93,5 @@ Route::get('/dashboard', function() {
 
 Route::get('/dashboard/posts/checkSlug', [DashboardController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategryController::class)->except('show')->middleware('admin');
